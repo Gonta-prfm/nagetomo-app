7915a0ef0329290a7bcf5e02aaaf4d7d14d904f4
Gonta-prfm <k.0426.s.46@icloud.com>
2021-03-16 14:39:29 +0900

    userモデルバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :profile,  length: {maximum: 200}
+
+  with_options presence: true do
+    validates :nickname
+    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i}
+  end
 end
