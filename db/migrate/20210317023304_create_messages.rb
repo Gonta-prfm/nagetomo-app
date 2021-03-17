a0020f610c336fe63119855c2a4866234bf3cf66
Gonta-prfm <k.0426.s.46@icloud.com>
2021-03-17 11:37:25 +0900

    messageモデルマイグレーションファイル編集

--- db/migrate/20210317023304_create_messages.rb
+++ db/migrate/20210317023304_create_messages.rb
@@ -1,7 +1,9 @@
 class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
-
+      t.string     :content, null: false
+      t.references :user,    null: false
+      t.references :room,    null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_094501) do
+ActiveRecord::Schema.define(version: 2021_03_17_023304) do
+
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content", null: false
+    t.bigint "user_id", null: false
+    t.bigint "room_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
