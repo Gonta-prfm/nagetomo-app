144ef0048862753aa02e9ad973e350db939002ff
Gonta-prfm <k.0426.s.46@icloud.com>
2021-03-17 11:50:00 +0900

    messageルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
    root 'rooms#index'
-  resources :rooms, only: [:index, :show, :new, :create]
+  resources :rooms, only: [:index, :show, :new, :create] do
+    resources :messsages, only: [:create]
+  end
 end
