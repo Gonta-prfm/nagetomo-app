510bb990df8dc084c9b741bd325907d17722ed6a
Gonta-prfm <k.0426.s.46@icloud.com>
2021-03-19 16:43:03 +0900

    message表示、保存時のエラー解消

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,7 @@ class MessagesController < ApplicationController
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
-    if message.save
+    if @message.save
       redirect_to room_messages_path(@room)
     else 
       @messages = @room.messages.includes(:user)
@@ -19,6 +19,6 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.(:message).permit(:content).merge(room_id: @room.id)
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
