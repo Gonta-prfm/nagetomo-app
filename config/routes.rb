01ef5dd8db0226d2fd7fd7acd25ffb183d8720da
Gonta-prfm <k.0426.s.46@icloud.com>
2021-03-20 15:04:51 +0900

    ActiveCableの有効化

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  mount ActiveCable.server => '/cable'
    root 'rooms#index'
-   get 'room/index'
   resources :rooms, only: [:index, :new, :create] do
     resources :messages, only: [:index, :create]
   end
