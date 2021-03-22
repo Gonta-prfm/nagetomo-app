1258b22de163dae270f43a833b9950ccf578f0df
Gonta-prfm <k.0426.s.46@icloud.com>
2021-03-22 17:17:55 +0900

    Relationshipsコントローラーアクションを定義

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,29 @@
 class RelationshipsController < ApplicationController
+  before_acition :set_user
+
+  def create
+    following = current_user.follow(@user)
+    if following.save
+      flash[:success] = "ユーザーをフォローしました"
+      redirect_to @user
+    else
+      flash[:alert] = "ユーザーのフォローに失敗しました"
+      redirect_to @user
+    end
+  end
+
+  def destroy
+    following = current_user.unfollow(@user)
+    if following.destroy
+      flash[:success] = "ユーザーのフォローを解除しました"
+      redirect_to @user
+    else
+      flash[:alert] = "ユーザーのフォロー解除に失敗しました"
+      redirect_to @user
+    end
+
+    private
+    def set_user
+      @user = User.find(params[:relationship][:follow_id])
+    end
 end
