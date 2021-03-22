529f9cdbaeaa717bf79ced5a9424a8f4470678d1
Gonta-prfm <k.0426.s.46@icloud.com>
2021-03-22 16:44:00 +0900

    アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,16 @@ class User < ApplicationRecord
   has_many :rooms, through: :room_users
   has_many :messages
 
+  #ユーザーがフォローしているユーザーとのアソシエーション
+  has_many :relationships, foreign_key: "user_id", dependent: :destroy
+  has_many :follewings, through: :relationships, source: :follow
+
+  #ユーザーをフォローしてくれているユーザーとのアソシエーション
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "follow_id", dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :user
+
+  
   validates :profile,  length: {maximum: 200}
 
   with_options presence: true do
